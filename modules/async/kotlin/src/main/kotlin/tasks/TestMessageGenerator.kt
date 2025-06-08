58c92ef3887b66160427f8b9a762406ab3d3beee
Scott Wilson <sw@nw2s.net>
2025-06-08 07:47:25 -0400

    Add Dockerfile, logging, and Redis queue management
    Introduce a Dockerfile to containerize the application using Amazon Corretto JDK. Enhance `TestMessageGenerator` with scheduled logging of Redis queue size and improve comments for better readability of message operations. Update `.idea` configuration files to include Gradle migration settings.

--- modules/async/kotlin/src/main/kotlin/tasks/TestMessageGenerator.kt
+++ modules/async/kotlin/src/main/kotlin/tasks/TestMessageGenerator.kt
@@ -36,6 +36,48 @@ class TestMessageGenerator(private val commands: RedisCommands<String, String>)
     private val mapper = ObjectMapper()
     private val dataNameList = ConcurrentHashMap.newKeySet<String>()
 
+    /**
+     * Logs the current size of the Redis queue named `benchmarkQueue` to the application logger.
+     * This method is scheduled to run at fixed intervals specified by the `@Scheduled` annotation.
+     *
+     * Functionality:
+     * - Retrieves the size of the queue `benchmarkQueue` from the Redis data store.
+     * - Outputs the queue size as an informational log message.
+     *
+     * Scheduling Details:
+     * - Initial delay: 10,000 milliseconds (10 seconds) before the method runs for the first time.
+     * - Fixed rate: 60,000 milliseconds (1 minute) between consecutive executions.
+     *
+     * Dependencies:
+     * - Accesses the `commands` field to execute Redis operations.
+     * - Uses the `logger` field to log the queue size.
+     */
+    @Scheduled(initialDelay = 10000, fixedRate = 60000)
+    private fun logQueueSize()
+    {
+        val queueSize = commands.llen("benchmarkQueue")
+        logger.info { "Current queue size: $queueSize" }
+    }
+
+    /**
+     * Generates messages and adds them to a Redis queue named `benchmarkQueue`.
+     *
+     * Functionality:
+     * - Checks the current size of the queue using the `llen` command.
+     * - Ensures that the queue contains at least 1000 messages.
+     * - If the queue size is below the threshold, generates the required number of test messages.
+     * - Serializes each generated message into a JSON string and pushes it into the queue using the `lpush` command.
+     *
+     * Scheduling Details:
+     * - The method is scheduled to run periodically.
+     * - Initial delay: 5000 milliseconds (5 seconds) before the method executes for the first time.
+     * - Fixed rate: 10000 milliseconds (10 seconds) between consecutive executions.
+     *
+     * Dependencies:
+     * - Utilizes the `commands` field to interact with the Redis data store.
+     * - Employs the `mapper` field to serialize messages into JSON format.
+     * - Uses the `logger` field to log information regarding the message generation process.
+     */
     @Scheduled(initialDelay = 5000, fixedRate = 10000)
     private fun generateMessage()
     {
@@ -80,10 +122,12 @@ class TestMessageGenerator(private val commands: RedisCommands<String, String>)
         when (Random.nextInt(0, 100))
         {
             in 0..50 -> {
+                /* Most often, we just want to read a random data item */
                 /* Get a random data name and create a ReadMessage for it */
                 return MessageWrapper(MessageType.READ_MESSAGE, System.currentTimeMillis(), ReadDataMessage(dataNameList.random()))
             }
             in 51..75 -> {
+                /* 25% of the time we want to update a random data item */
                 /* Get a random data name and create an UpdateMessage for it */
                 val randomOriginal = String.format("%02x", Random.nextInt(0, 256))
                 val randomReplace = String.format("%02x", Random.nextInt(0, 256))
@@ -91,12 +135,15 @@ class TestMessageGenerator(private val commands: RedisCommands<String, String>)
                 return MessageWrapper(MessageType.UPDATE_MESSAGE, System.currentTimeMillis(), UpdateDataMessage(dataNameList.random(), randomOriginal, randomReplace))
             }
             in 76..89 -> {
+                /* 25% of the time we want to create a new data item */
                 val dataName = UUID.randomUUID().toString()
                 dataNameList.add(dataName)
                 return MessageWrapper(MessageType.CREATE_MESSAGE, System.currentTimeMillis(), CreateDataMessage(dataName, Random.nextInt(1000, 10000)))
 
             }
             else -> {
+                /* The remaining 14% of the time we want to delete a data item */
+                /* Get a random data name and create a DeleteMessage for it */
                 return MessageWrapper(MessageType.DELETE_MESSAGE, System.currentTimeMillis(), DeleteDataMessage(dataNameList.random()))
             }
         }
